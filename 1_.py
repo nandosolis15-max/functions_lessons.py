e90fc06360c1da3ab08d1c27ce897eecaf041a09
nandosolis15-max <nandosolis15@icloud.com>
2026-01-26 18:54:48 +0000

    returns

--- 1_.py
+++ 1_.py
@@ -1,3 +1,64 @@
+#function = A block of reusable code
+#place () after the function name to invoke it
+
+# def happy_birthday(name,age):
+#     print(f"happy birthday to {name}!")
+#     print(f"you are {age}!")   
+#     print("happy birhtday to you!")
+#     print()
+
+# happy_birthday("Bro", 16)
+# happy_birthday("Roni", 17)
+# happy_birthday("Angie", 18)
+
+
+
+
+# def display_invoice(username, amount, due_date):
+#     print(f"hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+# display_invoice("RoniB", 100.1, "01/02")
+
+
+
+#return = statement used to end a function
+#and send a result back to the caller
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x, y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+
+
+
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name ("bro", "code")
+
+print(full_name)
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
